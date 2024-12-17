d8178b01d2b0592daf42c8a9ae1d2a02cb79d6b0
Dzmitry Kopats <dzmitry.kopats@innowise.com>
2024-12-17 12:51:59 +0100

    Create empty project

---
+++ ShipsApp/ShipsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShipsApp
+//
+//  Created by Dzmitry Kopats on 17/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
